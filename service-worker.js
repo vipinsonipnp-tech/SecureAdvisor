8a7db3cc5350d3584b3f2e0d69dec7611e8a51aa
vipinsonipnp-tech <vipinsonipnp@gmail.com>
2026-08-17 06:55:15 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+// This app no longer uses a service worker. This file exists only to clean up
+// the old one still installed in some browsers from an earlier version of the app.
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((key) => caches.delete(key)));
+      await self.registration.unregister();
+      const clientsList = await self.clients.matchAll({ type: 'window' });
+      clientsList.forEach((client) => client.navigate(client.url));
+    })()
+  );
+});
